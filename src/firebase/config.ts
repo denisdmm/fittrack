4efe5e27aa8ef5fff1efa355351f6b82e26d2f57
Denis M. M <denisdmm@gmail.com>
2025-11-18 00:59:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6730412679-750b7",
+  "appId": "1:1061617650558:web:cc3318e28048befeed012f",
+  "apiKey": "AIzaSyBiwbNGyHzUPZByq_2fy0KCaNICecZzn2w",
+  "authDomain": "studio-6730412679-750b7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1061617650558"
+};
